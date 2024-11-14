340383e4331bf75b42b9d307de0fe1d7786ce526
Suraj <surajfrnzy@gmail.com>
2024-11-14 13:50:31 +0000

    Implement The Flipping Logic and Game Winning Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react"
-import Game from "./components/Game"
-
+import Game from "./components/Game";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-     <Game />
+      <Game />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,107 +1,128 @@
 import React, { useEffect, useState } from "react";
+
 const Game = () => {
   const [gridSize, setGridSize] = useState(4);
   const [cards, setCards] = useState([]);
-
   const [flipped, setFlipped] = useState([]);
+  const [won, setWon] = useState(false);
   const [solved, setSolved] = useState([]);
   const [disabled, setDisabled] = useState(false);
-  const [won, setWon] = useState(false);
+  const [moves, setMoves] = useState(0);
 
   const initializeGame = () => {
     const totalCards = gridSize * gridSize;
     const totalPairs = Math.floor(totalCards / 2);
-    const numbers = Array(totalPairs)
+    const cardArr = Array(totalPairs)
       .fill(0)
-      .map((_, idx) => idx + 1);
-    const shuffledCards = [...numbers, ...numbers]
-      .sort(() => Math.random() - 0.5)
-      .map((number, idx) => ({
-        id: idx,
-        number,
-      }));
-
+      .map((_, idx) => {
+        return idx + 1;
+      });
+    const shuffledCards = [...cardArr, ...cardArr].sort(
+      () => Math.random() - 0.5
+    );
     setCards(shuffledCards);
     setFlipped([]);
     setSolved([]);
     setWon(false);
+    setMoves(0);
   };
-
+  const isFlipped = (idx) => {
+    return flipped.includes(idx);
+  };
   const resetGame = () => {
     initializeGame();
   };
 
-  const handleCardClick = (card) => {
-    if (disabled || won) return;
-    if (flipped.length == 0) {
-      setFlipped([card]);
+  const handleClick = (idx) => {
+    if (disabled || isFlipped(idx) || isSolved(idx)) return;
+    const newFlipped = [...flipped, idx];
+    setFlipped(newFlipped);
+    if (newFlipped.length == 2) {
+      const [firstIndex, secondIndex] = newFlipped;
+      setMoves((moves) => moves + 1);
+      if (cards[firstIndex] == cards[secondIndex]) {
+        setSolved([...solved, firstIndex, secondIndex]);
+        // console.log(moves);
+      }
+      setDisabled(true);
+      setTimeout(() => {
+        setFlipped([]);
+        setDisabled(false);
+      }, 1000);
     }
-    if (card == flipped[0]) {
-      setSolved([...solved, card, flipped[0]]);
-    }
-    setFlipped([...flipped, card]);
-  };
-
-  const isFlipped = (card) => {
-    return flipped.includes(card);
   };
 
   const isSolved = (card) => {
-    solved.includes(card);
+    return solved.includes(card);
   };
 
   useEffect(() => {
     initializeGame();
   }, [gridSize]);
 
+  useEffect(() => {
+    if (solved.length == cards.length && cards.length > 0) {
+      setWon(true);
+    }
+  }, [solved, cards]);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-grey-100 p-4">
-      <h1 className="text-3xl font-bold mb-6">Do You Remember</h1>
+    <div
+      className={
+        "flex flex-col items-center justify-center min-h-screen p-4 bg-gray-200"
+      }
+    >
+      {/* Header */}
+      <h1 className={`text-3xl font-bold mb-4`}>M3MORY</h1>
+      {/* Input */}
       <div>
-        <label htmlFor="gridSize" className="mr-6">
-          Grid Size (max 10) :
-        </label>
+        <label htmlFor="gridSize">Grid Size :</label>
         <input
           type="number"
-          className="border-2 p-2 mb-4 rounded-full border-gray-300"
-          value={gridSize}
           id="gridSize"
-          min="2"
-          max="10"
+          className={`border-2 border-solid border-gray-600 ml-3 rounded-full p-2`}
+          value={gridSize}
           onChange={(e) => setGridSize(e.target.value)}
         />
       </div>
-
       {/* Board */}
-      <div className={`grid gap-2 mb-4 grid-cols-${gridSize}`}>
-        {cards.map((card) => {
+      <div
+        className={`grid  gap-2 mt-5 mb-5`}
+        style={{ gridTemplateColumns: `repeat(${gridSize},1fr)` }}
+      >
+        {cards.map((card, idx) => {
           return (
             <div
-              key={card.id}
-              className={`flex items-center justify-center cursor-pointer h-12 w-12 rounded-md transition-all 
-                duration-500 text-xl font-bold p-10 hover:bg-gray-400 ${
-                  isSolved(card)
-                    ? "bg-green-400 text-white"
-                    : isFlipped(card)
-                    ? "bg-blue-400 text-white"
-                    : "bg-gray-200 text-gray-700"
-                }`}
-              onClick={() => handleCardClick(card)}
+              onClick={() => handleClick(idx)}
+              key={idx}
+              className={`aspect-square flex justify-center items-center cursor-pointer transition-all duration-600 text-xl font-bold p-10 hover:bg-gray-400 rounded-lg
+               ${
+                 isFlipped(idx) || isSolved(idx)
+                   ? isSolved(idx)
+                     ? "bg-green-400 text-white"
+                     : "bg-blue-500 text-white"
+                   : "bg-gray-300 text-gray-500"
+               } 
+                `}
             >
-              {flipped.includes(card) || solved.includes(card)
-                ? card.number
-                : "?"}
+              {isFlipped(idx) || isSolved(idx) ? card : "?"}
+              {/* {console.log(isFlipped({ card, idx }))} */}
             </div>
           );
         })}
       </div>
-
-      {/* Reset or Play Again*/}
+      {/* Result */}
+      {won ? (
+        <div className="mt-4 text-green-600 text-5xl font-bold animate-bounce ">
+          You have Won with {moves} Moves
+        </div>
+      ) : null}
+      {/* Reset Button */}
       <button
-        className="bg-green-700 p-3 rounded-full"
+        className={` bg-blue-600 rounded-full p-3 text-white font-bold mt-3 `}
         onClick={() => resetGame()}
       >
-        Reset
+        {won ? "Play Again" : "Reset"}
       </button>
     </div>
   );
